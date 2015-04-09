a4ee8c69b8edf00f5f240332058cda97935b5625
patrick nsukami <ndkpatt@gmail.com>
2015-04-09 00:10:52 +0000

    another introduction to asyncio (2)

--- another_intro.py
+++ another_intro.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import asyncio
- 
+
 @asyncio.coroutine
-def my_coroutine(seconds_to_sleep=3):
-    print('my_coroutine sleeping for: {0} seconds'.format(seconds_to_sleep))
+def my_coroutine(task_name, seconds_to_sleep=3):
+    print('{0} sleeping for: {1} seconds'.format(task_name, seconds_to_sleep))
+    # sleep: Coroutine that completes after a given time in sec.
+    # during sleeping, another task will be launched
     yield from asyncio.sleep(seconds_to_sleep)
- 
- 
+    print('{0} is finished'.format(task_name))
+
+tasks = [
+    # async: wrap coroutine in a future
+    asyncio.async(my_coroutine('task1', 4)),
+    # Task: a coroutine wrapped in a Future
+    asyncio.Task(my_coroutine('task2', 3)),
+    asyncio.Task(my_coroutine('task3', 2))
+]
+
 loop = asyncio.get_event_loop()
 loop.run_until_complete(
-    asyncio.gather(asyncio.Task(my_coroutine()))
+    # wait: return a future aggregating results from given coroutines/futures
+    asyncio.wait(tasks)
 )
 loop.close()
-

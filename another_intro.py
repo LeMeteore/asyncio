e7240f3b2605191fd2786732e21a786a753e7a04
patrick nsukami <ndkpatt@gmail.com>
2015-04-12 23:11:44 +0000

    another introduction to asyncio (5)

--- another_intro.py
+++ another_intro.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import asyncio
+import aiohttp
 
 @asyncio.coroutine
 def my_coroutine(future, task_name, seconds_to_sleep=3):
@@ -11,31 +12,46 @@ def my_coroutine(future, task_name, seconds_to_sleep=3):
     # set_result: mark the future done and set its result
     future.set_result('{0} is finished'.format(task_name))
 
+@asyncio.coroutine
+def fetch_page(url, pause=False):
+    # pausing one call will not block other calls
+    if pause:
+        yield from asyncio.sleep(2)
+    response = yield from aiohttp.request('GET', url)
+    assert response.status == 200
+    content = yield from response.read()
+    #print('url: {0}: content: {1}'.format(url, content))
+    print('url: {0}'.format(url))
 
 # print the result kept by the future
-def got_result(future):
+# def got_result(future):
     # result: return the result this future represent
     # can also be CancelledError, InvalidStateError, or an Exception if set
-    print(future.result())
+    # print(future.result())
 
 # an object that will contain something :)
-future1 = asyncio.Future()
-future2 = asyncio.Future()
-future3 = asyncio.Future()
+# future1 = asyncio.Future()
+# future2 = asyncio.Future()
+# future3 = asyncio.Future()
 
 # Add a callback to be run when the future becomes done
-future1.add_done_callback(got_result)
-future2.add_done_callback(got_result)
-future3.add_done_callback(got_result)
+# future1.add_done_callback(got_result)
+# future2.add_done_callback(got_result)
+# future3.add_done_callback(got_result)
+
+# tasks = [
+#     # async: wrap coroutine in a future
+#     asyncio.async(my_coroutine(future1, 'task1', 4)),
+#     # Task: a coroutine wrapped in a Future
+#     asyncio.Task(my_coroutine(future2, 'task2', 3)),
+#     asyncio.Task(my_coroutine(future3, 'task3', 2))
+# ]
 
 tasks = [
-    # async: wrap coroutine in a future
-    asyncio.async(my_coroutine(future1, 'task1', 4)),
-    # Task: a coroutine wrapped in a Future
-    asyncio.Task(my_coroutine(future2, 'task2', 3)),
-    asyncio.Task(my_coroutine(future3, 'task3', 2))
-]
-
+    asyncio.async(fetch_page('http://google.com', True)),
+    asyncio.async(fetch_page('http://cnn.com', True)),
+    asyncio.async(fetch_page('http://twitter.com'))
+    ]
 
 def run():
     loop = asyncio.get_event_loop()
@@ -44,6 +60,8 @@ def run():
         asyncio.wait(tasks)
         )
     loop.close()
+    for t in tasks:
+        print(t)
 
 if __name__ == '__main__':
     run()
@@ -64,3 +82,9 @@ if __name__ == '__main__':
 # >>> l = asyncio.get_event_loop()
 # >>> l.run_until_complete(asyncio.wait(t))
 # ({<Task finished coro=<corou() done, defined at <stdin>:1> result=None>, <Task finished coro=<corou() done, defined at <stdin>:1> result=None>}, set())
+# >>> future1 = asyncio.Future()
+# >>> future2 = asyncio.Future()
+# >>> help(future1)
+
+# >>> type(future1)
+# <class 'asyncio.futures.Future'>

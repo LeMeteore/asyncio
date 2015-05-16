f3ace006835715f99d1eecdb44daf939a92caf35
patrick nsukami <ndkpatt@gmail.com>
2015-05-16 05:03:31 +0000

    Power of coroutine (2)

--- power_of_coroutines.py
+++ power_of_coroutines.py
@@ -13,3 +13,24 @@ print(type(gen))
 # iteration over a generator
 for elt in gen:
     print(elt)
+
+
+# another example
+
+def count():
+    i = 0
+    # no problem even with an infinite loop
+    while True:
+        print("tick")
+        i += 1
+        yield i
+
+gen = count()
+
+# because of the while, let's break after 20 iterations
+for elt in gen:
+    if elt == 20:
+        print("done")
+        break
+else:
+    print(elt)

ea3ee5130aac74f8b32f2b98a107111ed2c57145
patrick nsukami <ndkpatt@gmail.com>
2015-05-25 22:46:09 +0000

    a function that returns factors & number of times they appear + a divisor generator.

--- primes.py
+++ primes.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""" Un nombre premier est un entier naturel qui admet exactement deux diviseurs distincts entiers et positifs (qui sont alors 1 et lui-même). Ainsi, 1 n'est pas premier car il n'a qu'un seul diviseur entier positif ; 0 non plus car il est divisible par tous les entiers positifs.'
-"""
 
 from itertools import count, islice
 from math import sqrt
+from functools import reduce
+from collections import Counter
+
+
+""" Un nombre premier est un entier naturel qui admet exactement deux diviseurs distincts entiers et positifs (qui sont alors 1 et lui-même). Ainsi, 1 n'est pas premier car il n'a qu'un seul diviseur entier positif ; 0 non plus car il est divisible par tous les entiers positifs.'
+"""
 
 def is_prime(n):
     return all(n%i for i in range(2, n))
@@ -60,6 +64,22 @@ def primes_factors(n=100):
         d+=1
     return f
 
+# returns factors & number of time they appears
+def primes_factors2(n=100):
+    f = list()
+    ff = list()
+    d = 2;
+    if is_prime(n): return [1, n]
+    while (n>1):
+        while n%d==0:
+            f.append(d)
+            n/=d
+        d+=1
+    # ff is a list of tuples (factor, time)
+    for i in Counter(f).items():
+        ff.append(i)
+    return ff
+
 def primes_factors_generator(n=100):
     f = list()
     d = 2;
@@ -80,11 +100,32 @@ def divisors(n=100):
     return d
 
 def divisors_generator(n=100):
-    for i in range(2, int(n/2+1)):
+    for i in range(1, int(n/2+1)):
         if n%i == 0: yield i
+    # n is a divisor of n
+    yield n
 
 def divisors_generator1(n=100):
     for d in iter(divisors(n)): yield d
+
+def divisorGen(n=100):
+    #factors = list(primes_factors_generator(n))
+    factors = primes_factors2(n)
+    nfactors = len(factors)
+    f = [0] * nfactors
+    while True:
+        yield reduce(lambda x, y: x*y, [factors[x][0]**f[x] for x in range(nfactors)], 1)
+        i = 0
+        while True:
+            f[i] += 1
+            if f[i] <= factors[i][1]:
+                break
+            f[i] = 0
+            i += 1
+            if i >= nfactors:
+                return
+
+###
 
 def is_divisor(a, b):
     divide = False
